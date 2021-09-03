ee32fb97176b3d497845a5c5161366f653d1327f
Li HN <lhn19990113@gmail.com>
2021-09-03 19:26:06 +0800

    Modified the movement

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     robot_li = agent.robot()
 
     return_saving = np.zeros((param.ENV_SETTINGS.MATRIX_SIZE, param.ENV_SETTINGS.MATRIX_SIZE))
-    for iteration in range(0, 50000):
+    for iteration in range(0, 20000):
         # prediction
         # robot initialization
         robot_li.obser, robot_li.pos = env.reset()
--- parameters.py
+++ parameters.py
@@ -9,13 +9,13 @@ class ENV_SETTINGS:
                                          [-1, -1,  0,  0,  1, -1],
                                          [-1, -1, -1, -1, -1, -1]])            # trap, walls: -1, goal: 1
     TEN_BY_TEN_TEST          = np.zeros((12, 12))
-    MATRIX_SIZE              = 12                                              # map size + walls
+    MATRIX_SIZE              = 6                                              # map size + walls
     VALUE_ARRAY              = np.zeros((MATRIX_SIZE, MATRIX_SIZE))
 
 
 class AGENT_ACTION:
-    ACTION_SPACE             = [[0, 1], [-1, 0], [0, -1], [1, 0]]              # East, South, West, North
+    ACTION_SPACE             = [[0, 1], [1, 0], [0, -1], [-1, 0]]              # East, South, West, North
     ACTION_PROB_INT          = 0.25
-    ACTION_REWARD            = 0.7
-    DISCOUNT_FACTOR          = 0.8
-    EPSILON                  = 0.5
+    ACTION_REWARD            = 0.2
+    DISCOUNT_FACTOR          = 0.9
+    EPSILON                  = 0.8
